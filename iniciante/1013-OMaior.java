370e7331bc37b21590e8f776bd5bc5c9adbff7aa
Maximillian Benjamin Vicente <maximillianben100@gmail.com>
2026-09-13 13:17:47 -0300

    feat: solve 1013 - O Maior (beecrowd)

---
+++ iniciante/1013-OMaior.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        int maiorAB = (a + b + Math.abs(a - b)) / 2;
+        int maior = (maiorAB + c + Math.abs(maiorAB - c)) / 2;
+
+        System.out.println(maior + " eh o maior");
+
+        scanner.close();
+    }
+}
